90c73c666a608e27939f698bdbc5988976ad128a
Pedro Figueiredo <pedrom0@hotmail.com>
2019-09-18 23:09:28 +0100

    Add History API

--- index.js
+++ index.js
@@ -1,76 +1,107 @@
-var language = "en";
-var webVisible = false;
-var animVisible = false;
-var jogosVisible = false;
-var grafVisible = false;
-var uiVisible = false;
 
-function showWeb(){
-	if(!webVisible){
+initialize();
+
+function initialize() {
+	window.addEventListener('popstate', changeState);
+
+	if (history.state == null) {
+		history.replaceState("home", null, null);
+	}
+
+	changeState();
+}
+
+function changeState() {
+
+	if (history.state == "home") {
+
+		document.getElementById("webCanvas").style.display = "none";
+		document.getElementById("animCanvas").style.display = "none";
+		document.getElementById("jogosCanvas").style.display = "none";
+		document.getElementById("grafCanvas").style.display = "none";
+		document.getElementById("uiCanvas").style.display = "none";
+		document.getElementById("menu").style.display = "inherit";
+		document.getElementById("cover").style.visibility = "hidden";
+
+	} else if (history.state == "web") {
+
 		document.getElementById("webCanvas").style.display = "inherit";
 		document.getElementById("menu").style.display = "none";
 		document.getElementById("cover").style.visibility = "visible";
-		webVisible = true;
+
+	} else if (history.state == "anim") {
+
+		document.getElementById("animCanvas").style.display = "inherit";
+		document.getElementById("menu").style.display = "none";
+		document.getElementById("cover").style.visibility = "visible";
+
+	} else if (history.state == "graf") {
+
+		document.getElementById("grafCanvas").style.display = "inherit";
+		document.getElementById("menu").style.display = "none";
+		document.getElementById("cover").style.visibility = "visible";
+
+	} else if (history.state == "jogos") {
+
+		document.getElementById("jogosCanvas").style.display = "inherit";
+		document.getElementById("menu").style.display = "none";
+		document.getElementById("cover").style.visibility = "visible";
+
+	} else if (history.state == "ui") {
+
+		document.getElementById("uiCanvas").style.display = "inherit";
+		document.getElementById("menu").style.display = "none";
+		document.getElementById("cover").style.visibility = "visible";
+
+	}
+}
+
+function showWeb(){
+	if (history.state == "home"){
+		history.pushState("web", null, null);
+		changeState();
 	} else {
-		document.getElementById("webCanvas").style.display = "none";
-		document.getElementById("menu").style.display = "inherit";
-		document.getElementById("cover").style.visibility = "hidden";
-		webVisible = false;
+		history.back();
+		changeState();
 	}
 }
 
 function showAnim(){
-	if(!animVisible){
-		document.getElementById("animCanvas").style.display = "inherit";
-		document.getElementById("menu").style.display = "none";
-		document.getElementById("cover").style.visibility = "visible";
-		animVisible = true;
+	if (history.state == "home"){
+		history.pushState("anim", null, null);
+		changeState();
 	} else {
-		document.getElementById("animCanvas").style.display = "none";
-		document.getElementById("menu").style.display = "inherit";
-		document.getElementById("cover").style.visibility = "hidden";
-		animVisible = false;
+		history.back();
+		changeState();
 	}
 }
 
 function showJogos(){
-	if(!jogosVisible){
-		document.getElementById("jogosCanvas").style.display = "inherit";
-		document.getElementById("menu").style.display = "none";
-		document.getElementById("cover").style.visibility = "visible";
-		jogosVisible = true;
+	if (history.state == "home"){
+		history.pushState("jogos", null, null);
+		changeState();
 	} else {
-		document.getElementById("jogosCanvas").style.display = "none";
-		document.getElementById("menu").style.display = "inherit";
-		document.getElementById("cover").style.visibility = "hidden";
-		jogosVisible = false;
+		history.back();
+		changeState();
 	}
 }
 
 function showGraf(){
-	if(!grafVisible){
-		document.getElementById("grafCanvas").style.display = "inherit";
-		document.getElementById("menu").style.display = "none";
-		document.getElementById("cover").style.visibility = "visible";
-		grafVisible = true;
+	if (history.state == "home"){
+		history.pushState("graf", null, null);
+		changeState();
 	} else {
-		document.getElementById("grafCanvas").style.display = "none";
-		document.getElementById("menu").style.display = "inherit";
-		document.getElementById("cover").style.visibility = "hidden";
-		grafVisible = false;
+		history.back();
+		changeState();
 	}
 }
 
 function showUI(){
-	if(!uiVisible){
-		document.getElementById("uiCanvas").style.display = "inherit";
-		document.getElementById("menu").style.display = "none";
-		document.getElementById("cover").style.visibility = "visible";
-		uiVisible = true;
+	if (history.state == "home"){
+		history.pushState("ui", null, null);
+		changeState();
 	} else {
-		document.getElementById("uiCanvas").style.display = "none";
-		document.getElementById("menu").style.display = "inherit";
-		document.getElementById("cover").style.visibility = "hidden";
-		uiVisible = false;
+		history.back();
+		changeState();
 	}
 }
