78110d8951e0ef19381173aab3d72a28a557e202
Pedro Figueiredo <pedrom0@hotmail.com>
2019-10-21 21:22:12 +0100

    Add 'Away'

---
+++ Away/js/engine.js
@@ -0,0 +1,83 @@
+//--------Global Variables--------
+
+var background;
+var player = null;
+
+var x=0;
+var y=0;
+var groundPosition;
+
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+
+
+//--------Functions--------
+
+function initialize(){
+	background = new Image();
+	background.src = "images/Fields.png";
+
+	player = new Player("images/blue.png", 64);
+
+	groundPosition = (224*2) - player.size;
+	
+	player.y = groundPosition;
+
+	main();
+}
+
+var render = function () {
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+
+	x = parseInt((window.innerWidth-background.width)/2);
+	y = parseInt((window.innerHeight-background.height)/2);
+
+	player.normalizedX = player.x + x;
+	player.normalizedY = player.y + y;
+
+	context.drawImage(background, x, y);
+	player.render();
+}
+
+var main = function () {
+
+	render();
+	requestAnimationFrame(main);
+};
+
+
+//--------Tools--------
+
+var animate = function(spritesheet, object, width, height, numberFrames){
+	context.drawImage(spritesheet, width*object.clipPosition, object.state*height, width, height, object.normalizedX, object.normalizedY, width, height);
+	object.currentFrame++;
+	if(object.currentFrame%object.frameRate===0){
+		object.clipPosition++;
+		object.currentFrame=0;
+	}
+	if (object.clipPosition>numberFrames-1){
+		object.clipPosition=0;
+	}
+}
+
+//Keyboard controls
+var keysDown = {};
+
+addEventListener("keydown", function (e) {
+	keysDown[e.keyCode] = true;
+}, false);
+
+addEventListener("keyup", function (e) {
+	delete keysDown[e.keyCode];
+}, false);
+
+
+//--------Cross-browser support for requestAnimationFrame--------
+var w = window;
+requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+context.imageSmoothingEnabled = false;
+
+
+//--------Initialize-------
+initialize();
---
+++ Away/js/player.js
@@ -0,0 +1,83 @@
+var Player = function(spritesheet, size){
+	this.size = size;
+
+	this.x = 500;
+	this.y = 0;
+
+	this.maximumVelocityX = 5;
+	this.accelerationX = 2;
+	this.velocityX = 0;
+
+	this.jumping = false;
+	this.jumpStopFactor = 3;
+	this.gravity = 0.5;
+	this.impulseY = 15;
+	this.terminalVelocityY = 15;
+	this.velocityY = 0;
+	
+	this.frameRate = 5; //12 FPS
+	this.currentFrame = 0;
+	this.clipPosition = 0;
+	this.state = 0; // 0: Idle | 1: Run | 2: Jump | 3: Block
+	this.numberFrames = 7;
+
+	this.spritesheetReady = false;
+	this.playerSpritesheet = new Image();
+
+	this.playerSpritesheet.onload = function () {
+		this.spritesheetReady = true;
+	};
+
+	this.playerSpritesheet.src = spritesheet;
+
+	this.render = function(){
+		this.control();
+		this.x += this.velocityX;
+
+		animate(this.playerSpritesheet, this, this.size, this.size, this.numberFrames);
+	}
+
+	this.control = function(){
+
+		//Lateral Movement
+
+		if (39 in keysDown){ //Right Arrow
+			if(this.velocityX < this.maximumVelocityX){
+				this.velocityX += this.accelerationX;
+			}
+		} else if (37 in keysDown){ //Left Arrow
+			if(this.velocityX > -this.maximumVelocityX){
+				this.velocityX -= this.accelerationX;
+			}
+		} else { //Stabilize X
+			if(this.velocityX < 0){
+				this.velocityX += this.accelerationX;
+			} else if(this.velocityX > 0){
+				this.velocityX -= this.accelerationX;
+			}
+		}
+
+		//Vertical Movemement
+
+		if (90 in keysDown && !this.jumping){ //Z Key
+			this.velocityY = -this.impulseY;
+			this.jumping = true;
+		}
+
+		if(this.velocityY < 0 && !(90 in keysDown)){
+			this.velocityY -= this.velocityY/this.jumpStopFactor;
+		}
+
+		this.y += this.velocityY;
+
+		if (this.y > groundPosition){
+			this.y = groundPosition;
+			this.jumping = false;
+			this.velocityY = 0;
+		} else if (this.velocityY < this.terminalVelocityY){
+			this.velocityY += this.gravity;
+		}
+
+
+	}
+}
